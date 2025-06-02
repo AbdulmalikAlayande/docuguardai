1e953d2c1e1af5777e29d0ff432c678d28517037
AbdulmalikAlayande <alaabdulmalik03@gmail.com>
2025-06-02 21:04:25 +0100

    Initial Commit

---
+++ src/main/java/app/bola/docuguardai/DocuguardaiApplication.java
@@ -0,0 +1,13 @@
+package app.bola.docuguardai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocuguardaiApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(DocuguardaiApplication.class, args);
+	}
+	
+}
---
+++ src/test/java/app/bola/docuguardai/DocuguardaiApplicationTests.java
@@ -0,0 +1,13 @@
+package app.bola.docuguardai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocuguardaiApplicationTests {
+	
+	@Test
+	void contextLoads() {
+	}
+	
+}
